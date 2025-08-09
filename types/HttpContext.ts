c4df9940e69454d003ebe754586a48a32e454d62
Blacky Wolf <support@blackywolf.me>
2025-08-09 16:34:12 -0500

    Added state and params methods to HttpContext

--- types/HttpContext.ts
+++ types/HttpContext.ts
@@ -6,6 +6,26 @@ import { HttpResponse } from "./HttpResponse.ts";
  */
 export interface HttpContext {
     /**
+     * The parameters extracted from the request URL path.
+     * 
+     * @template Params The type of the parameters extracted from the request
+     *                  URL path.
+     * 
+     * @returns The parameters extracted from the URL path as an object.
+     */
+    getParams: <Params = any>() => Params;
+
+    /**
+     * State used to share data during the request lifecycle.
+     * 
+     * @template State The type of the state object used to share data during
+     *                 the request lifecycle.
+     * 
+     * @returns The state data.
+     */
+    getState: <State = any>() => State;
+
+    /**
      * The HTTP request.
      *
      * @see {@link HttpRequest}
